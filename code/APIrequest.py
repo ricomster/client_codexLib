c45f5395b8cddea7d69e712bc2ad8e7b4ed5850a
ricomster <zarchael123@gmail.com>
2022-05-25 16:28:18 +0700

    1

--- code/APIrequest.py
+++ code/APIrequest.py
@@ -31,7 +31,7 @@ s = requests.Session()
 
 # Authentification Login
 
-url = 'http://192.168.1.105:8080/api/'
+url = 'http://192.168.92.83:8080/api/'
 # url = 'http://localhost:8080/api/'
 
 def login(email,password):
